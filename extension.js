caedf966f3d0c9ca040c93d21e45e2eaab050f03
Lukas Vacek <lucas.vacek@gmail.com>
2011-12-17 00:16:06 +0000

    searchprovider extension stub

--- extension.js
+++ extension.js
@@ -1,3 +1,11 @@
+// inspired by https://github.com/brot/gnome-shell-extension-sshsearch/
+// licence GPLv3
+// author: Lukas Vacek <lucas.vacek@gmail.com>
+
+// Linux Mint 12 supported
+// uses apt-cache showpkg to search for packages
+// uses mint-make-cmd to install
+// /usr/share/gnome-shell/js/ui/search.js for API docs :-)
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
@@ -5,14 +13,44 @@ const Search = imports.ui.search;
 
 let appSearchProvider;
 
+// inherits from Searc.SearchProvider
+function AppSearchProvider() {
+	Search.SearchProvider.call(this,"AppSearch"); // call parent constructor
+}
+// inheritance
+AppSearchProvider.prototype = new Search.SearchProvider();
+AppSearchProvider.prototype.constructor = AppSearchProvider;
+// override methods
+AppSearchProvider.prototype.getInitialResultSet = function(terms) {
+	return [ "hello" ];
+}
+
+AppSearchProvider.prototype.getSubsearchResultSet = function(prevResults,terms) {
+	return this.getInitialResultSet(terms);
+}
+
+AppSearchProvider.prototype.getResultMeta = function(resultId) {
+	return {
+		'id':"123",
+		'name':"hi there!",
+		'createIcon': function(size) {
+			return new St.Icon({ 
+				'icon_type':St.IconType.FULLCOLOR,
+				'icon_size':size,
+				'icon_name':"system-software-install"
+			});
+		}
+	};
+}
+
 function init() {
-        appSearchProvider = Search.SearchProvider();
+	appSearchProvider = new AppSearchProvider();
 }
 
 function enable() {
-        Main.overview.addSearchProvider(appSearchProvider)
+        Main.overview.addSearchProvider(appSearchProvider);
 }
 
 function disable() {
-        Main.overview.removeSearchProvider(appSearchProvider)
+        Main.overview.removeSearchProvider(appSearchProvider);
 }

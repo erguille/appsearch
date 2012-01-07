5eb38395b0a57b720eec2340be5f70886b9a93fb
Lukas Vacek <lucas.vacek@gmail.com>
2012-01-07 20:52:27 +0000

    packagekit support

--- extension.js
+++ extension.js
@@ -2,8 +2,9 @@
 // licence GPLv3
 // author: Lukas Vacek <lucas.vacek@gmail.com>
 
-// Linux Mint 12 supported
+// Ububtu, Debian, Linux Mint 12, and PackageKit supported
 // uses apt-cache show to search for packages
+// If apt-cache is not available, packagekit used instead
 // uses mint-make-cmd to install
 // /usr/share/gnome-shell/js/ui/search.js for API docs :-)
 const St = imports.gi.St;
@@ -26,6 +27,7 @@ AppSearchProvider.prototype = {
 
     _init: function(title) {
         Search.SearchProvider.prototype._init.call(this, title);
+        this.mode = "apt";
     },
 
     getInitialResultSet: function(terms) { 
@@ -35,7 +37,17 @@ AppSearchProvider.prototype = {
                 return [];
             }
             let searched_app_name = terms[i];
-            let argv = ["apt-cache","-q=2","-n","show",searched_app_name];
+            this.searched_app_name = searched_app_name;
+            let argv;
+            if (this.mode == "apt") {
+                argv = ["apt-cache","-q=2","-n","show",searched_app_name];
+            } else if (this.mode == "pkcon") {
+                // 'get-depends' is slow (still sort of acceptable ...)
+                // 'resolve' is faster - but doesnt return exit code > 0 on fail
+                // plus we cant process its output because of 
+                // https://bugzilla.gnome.org/show_bug.cgi?id=667069
+                argv = ["pkcon","-p","get-depends",searched_app_name];
+            }
             let [success,pid] = 
                 GLib.spawn_async(null,
                    argv,null,
@@ -48,7 +60,9 @@ AppSearchProvider.prototype = {
             GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, function(pid, status) {
                 GLib.spawn_close_pid(pid);
                 if (status == 0) {
-                    _self.addItems([searched_app_name]);
+                    if (_self.searched_app_name == searched_app_name) {
+                        _self.addItems([searched_app_name]);
+                    }
                 }
             });
         }
@@ -75,13 +89,35 @@ AppSearchProvider.prototype = {
     },
 
     activateResult: function(resultId) {
-    	let args = ["apturl","apt://"+resultId];
+        let args;
+        if (this.mode == "apt") { 
+    	    args = ["apturl","apt://"+resultId];
+        } else if (this.mode == "pkcon") {
+            args = ["gpk-install-package-name",resultId];
+        }
 	    Util.spawn(args);
+    },
+
+    setMode: function(mode) {
+        this.mode = mode;
     }
 }
 
 function init() {
     appSearchProvider = new AppSearchProvider('APP SEARCH');
+    let mode = "apt";
+    let ret,stdout,stderr,which_apt_cache,which_apturl;
+    [ret,stdout,stderr,which_apt_cache] = GLib.spawn_command_line_sync("which apt-cache");
+    [ret,stdout,stderr,which_apturl] = GLib.spawn_command_line_sync("which apturl");
+    if (which_apt_cache != 0 || which_apturl != 0) {
+        let which_pkcon,which_gpk;
+        [ret,stdout,stderr,which_pkcon] = GLib.spawn_command_line_sync("which pkcon");
+        [ret,stdout,stderr,which_gpk] = GLib.spawn_command_line_sync("which gpk-install-package-name");
+        if (which_pkcon == 0 && which_gpk == 0) {
+            mode = "pkcon";
+        }
+    }
+    appSearchProvider.setMode(mode);
 }
 
 function enable() {
